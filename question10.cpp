ed83bcaa4af09e3afb2b45a4ce54ad9b00f43eaf
git03-khushi <khushimittal679@gmail.com>
2025-07-09 11:25:46 +0530

    Adding Questions

---
+++ question10.cpp
@@ -0,0 +1,26 @@
+// Fibonacci Sequence//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of terms in the Fibonacci sequence: ";
+    cin >> n;
+
+    int fib1 = 0;
+    int fib2 = 1;
+    int fibNext;
+
+    cout << "Fibonacci Sequence: ";
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << fib1 << " ";
+        fibNext = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fibNext;
+        
+    }
+}
