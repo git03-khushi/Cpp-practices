cb0cfad3331918c721fcb9c196b18bc27c925e49
git03-khushi <khushimittal679@gmail.com>
2025-07-09 10:58:24 +0530

    Adding Questions

---
+++ question7.cpp
@@ -0,0 +1,22 @@
+// Factorial//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    int factorial = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        factorial *= i;
+        cout << "Factorials are: " << factorial << endl;
+    }
+
+    cout << "Factorial of " << n << " = " << factorial << endl;
+
+    return 0;
+}
